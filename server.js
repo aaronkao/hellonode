e43ed4e7c4d9c01ea813ef678a623a35524fac89
Anton Podviaznikov <podviaznikov@gmail.com>
2014-12-31 14:09:50 -0800

    Update server.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 
 app.get('/', function(req, res){
-    res.send('Happy New Year!');
+    res.send('Happy New Year, Anton!');
 });
 
 app.listen(80);
